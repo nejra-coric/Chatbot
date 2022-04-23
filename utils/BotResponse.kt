5d565b0fe71cae2add3220ff3c5f49d7d901f69f
easycode7 <66847545+easycode7@users.noreply.github.com>
2022-04-23 11:25:30 +0200

    Add files via upload

---
+++ utils/BotResponse.kt
@@ -0,0 +1,96 @@
+package com.ncoric.chatbot.utils
+
+import com.ncoric.chatbot.utils.Constants.OPEN_GOOGLE
+import com.ncoric.chatbot.utils.Constants.OPEN_SEARCH
+import java.sql.Date
+import java.sql.Timestamp
+import java.text.SimpleDateFormat
+
+object BotResponse {
+
+    fun basicResponses(_message: String): String {
+
+        val random = (0..2).random()
+        val message =_message.toLowerCase()
+
+        return when {
+
+            //Flips a coin
+            message.contains("flip") && message.contains("coin") -> {
+                val r = (0..1).random()
+                val result = if (r == 0) "heads" else "tails"
+
+                "I flipped a coin and it landed on $result"
+            }
+
+            //Math calculations
+            message.contains("solve") -> {
+                val equation: String? = message.substringAfterLast("solve")
+                return try {
+                    val answer = SolveMath.solveMath(equation ?: "0")
+                    "$answer"
+
+                } catch (e: Exception) {
+                    "Sorry, I can't solve that."
+                }
+            }
+
+            //Hello
+            message.contains("hello") -> {
+                when (random) {
+                    0 -> "Hello there!"
+                    1 -> "Hi"
+                    2 -> "Hello"
+                    else -> "error" }
+            }
+
+            //How are you?
+            message.contains("how are you") -> {
+                when (random) {
+                    0 -> "I'm doing fine, thanks!"
+                    1 -> "I'm fine! How are you?"
+                    2 -> "Pretty good! How about you?"
+                    else -> "error"
+                }
+            }
+            //What are you doing?
+            message.contains("what are you doing") -> {
+                when (random) {
+                    0 -> "I'm solving some technical problems."
+                    1 -> "I'm talking with you."
+                    2 -> "I'm working! How about you?"
+                    else -> "error"
+                }
+            }
+
+            //What time is it?
+            message.contains("what time is it") && message.contains("time")-> {
+                val timeStamp = Timestamp(System.currentTimeMillis())
+                val sdf = SimpleDateFormat("dd/MM/yyyy HH:mm")
+                val date = sdf.format(Date(timeStamp.time))
+
+                date.toString()
+            }
+
+            //Open Google
+            message.contains("open") && message.contains("google")-> {
+                OPEN_GOOGLE
+            }
+
+            //Search on the internet
+            message.contains("search")-> {
+                OPEN_SEARCH
+            }
+
+            //When the programme doesn't understand...
+            else -> {
+                when (random) {
+                    0 -> "I don't understand..."
+                    1 -> "Try asking me something different"
+                    2 -> "I am not programmed to understand this question"
+                    else -> "error"
+                }
+            }
+        }
+    }
+}
